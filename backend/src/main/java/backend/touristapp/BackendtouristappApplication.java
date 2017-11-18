17e1b40f034203753d8ae7cf96f5414296cde94f
Artur Waliczek <artur.waliczek@gmail.com>
2017-11-18 19:24:17 +0100

    spring boot project added, created backend folder and frontend

---
+++ backend/src/main/java/backend/touristapp/BackendtouristappApplication.java
@@ -0,0 +1,12 @@
+package backend.touristapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtouristappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtouristappApplication.class, args);
+	}
+}
